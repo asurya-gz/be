08aa0f907495e468e171d47ffc332024f8bcd4f7
agsry <agungkdp208@gmail.com>
2024-01-03 13:47:43 +0700

    cdc

--- app.js
+++ app.js
@@ -61,6 +61,12 @@ app.use(
 app.options("*", (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://klinikkartika.up.railway.app"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
   res.status(200).end();
 });
 
