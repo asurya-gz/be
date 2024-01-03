bc15fe3bd44186aa8b2af794a143f06d039434e8
agsry <agungkdp208@gmail.com>
2024-01-03 10:06:26 +0700

    km

--- app.js
+++ app.js
@@ -48,12 +48,16 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://klinikkartika.up.railway.app"
+  ); // Replace with your actual frontend URL
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Add this line
   next();
 });
 
