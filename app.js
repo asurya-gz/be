1493fb22f4d25785aba640a7627b546dbad4cbdd
agsry <agungkdp208@gmail.com>
2024-01-03 10:03:14 +0700

    hb

--- app.js
+++ app.js
@@ -53,11 +53,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Sidebar active
-  res.locals.activeMenu = req.path;
-
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
